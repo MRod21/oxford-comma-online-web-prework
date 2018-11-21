45b0888c14289631a79faa32bde375f4ca87f681
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-11-21 06:12:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
-  if array.length == 2
+  if array.size == 2
+    array.join
     return "#{array[0]} and #{array[1]}"
   elsif array.length < 2 
     array[-1].insert(0, "and ")
-  end
-    array.join(" , ")
 end
